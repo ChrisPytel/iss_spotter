bc68455c1269ca245ea27082e4a24707e8a9be3a
ChrisPytel <chris_pytel@hotmail.com>
2024-02-26 23:57:58 -0500

    add function to fetch ip and cases for error handling from server

---
+++ index.js
@@ -0,0 +1,11 @@
+// index.js
+const { fetchMyIP } = require('./iss');
+
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:' , ip);
+});
--- iss.js
+++ iss.js
@@ -6,8 +6,28 @@
  *   - An error, if any (nullable)
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
-const fetchMyIP = function(callback) { 
+
+const request = require('request');
+
+
+const fetchMyIP = function(callback) {
   // use request to fetch IP address from JSON API
-}
+  request(`https://api.ipify.org?format=json`, (err, response, body) =>{
+    console.log(`Code is: `, response.statusCode);
+
+    if (err) {
+      return callback(error, null);      
+    }         
+    if (response.statusCode !== 200) {  // if non-200 status, assume server error
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+    return;
+    }
+    else {
+      const deSerialObj = JSON.parse(body).ip;
+      callback(null, deSerialObj);
+    }
+  });
+};
 
 module.exports = { fetchMyIP };
