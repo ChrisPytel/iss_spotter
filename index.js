88dba8e4b5807ed9d6f870023f50b4d50c174fd0
ChrisPytel <chris_pytel@hotmail.com>
2024-02-27 12:51:15 -0500

    add edge case if IP passed is invalid

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 // index.js
 const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
-    console.log("It didn't work!" , error);
+    console.log("It didn't work! Failed to get our IP" , error);
     return;
   }
+  console.log('It worked! Returned our IP:' , ip);
 
-  console.log('It worked! Returned IP:' , ip);
+
+  fetchCoordsByIP(ip, (err,data) => {
+    console.log(`fetchCoordsByIP error is:`, err);
+
+  if (err) {
+    console.log("It didn't convert our IP to Lat + Long!", err);
+    return;
+  }
+  else if (data) {
+    console.log("It returned out Lat + Long" , data, typeof data);
+    return;
+  }
+  });
 });
+
+
--- iss.js
+++ iss.js
@@ -30,4 +30,44 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+
+
+const fetchCoordsByIP = function (string, callback){
+  console.log(`Checking IPstring`, string);
+  request(`http://ipwho.is/5`, (err, response, body) =>{
+    console.log(`Code for ipwho is: `, response.statusCode);
+    console.log(`body is`, body);
+    if (err) {
+      return callback(error, null);      
+    }
+    if (response.statusCode !== 200) {  // if non-200 status, assume server error
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+    return;
+    }
+    // if we get a response from server which is 200 but doenst return a proper IP
+    const deSerialObj = JSON.parse(body);    
+    if (!deSerialObj.success) { // check if "success" is true or not, if returns a falsey conclusion, execute this code
+      const message = `Success status was ${deSerialObj.success}. Server message says: ${deSerialObj.message} when fetching for IP ${deSerialObj.ip}`;
+      callback(Error(message), null);
+      return;
+    }    
+    //If it DOES return a proper ip
+    else {
+      const coords = {
+        latitude: deSerialObj.latitude,
+        longitude: deSerialObj.longitude
+      }
+      callback(null, coords); //return no error and coordinates
+    }
+  });
+
+
+
+}
+
+
+module.exports = { 
+  fetchMyIP,
+  fetchCoordsByIP
+ };
