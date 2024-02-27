123385c9f5fd81f931637c5fcb1bdf60a610f4e3
ChrisPytel <chris_pytel@hotmail.com>
2024-02-26 23:17:44 -0500

    initial file setup

---
+++ iss.js
@@ -0,0 +1,13 @@
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+const fetchMyIP = function(callback) { 
+  // use request to fetch IP address from JSON API
+}
+
+module.exports = { fetchMyIP };
